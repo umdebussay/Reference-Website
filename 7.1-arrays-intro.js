a415f0e3e531a7d906886e131f6c747e6f7a9c34
umdebussay <126812150+umdebussay@users.noreply.github.com>
2023-03-07 17:38:05 +0000

    7.1

---
+++ 7.1-arrays-intro.js
@@ -0,0 +1,19 @@
+function example() {
+  var topStudents = ["Christian", "Kristian", "Shakka", "Jaime", "AJ"];
+  var name = topStudents[2];
+  console.log(name);
+  topStudents[0] = "Mauricio";
+  console.log(topStudents);
+  topStudents = "Mauricio";
+  console.log(topStudents);
+}
+
+function problem1() {
+var travelList = ["Rome", "Paris", "Australia", "England", "Canada"];
+var place = travelList[2];
+}
+
+
+function problem2(){
+
+}
