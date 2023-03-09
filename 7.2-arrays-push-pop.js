064e4314e12d2d56cc11d4698a8f119135c49da7
umdebussay <126812150+umdebussay@users.noreply.github.com>
2023-03-09 18:15:59 +0000

    finishthis

---
+++ 7.2-arrays-push-pop.js
@@ -0,0 +1,53 @@
+/* 
+  Warm-up
+  1. Create a global variable called favFoods and assign it to be an array strings that are your top three favorite foods.
+  2. Use console.log() to print out each element to the console individually
+*/
+var favFoods = ["IceCream", "Burgers", "Cookies"];
+console.log(favFoods[0]);
+console.log(favFoods[1]);
+console.log(favFoods[2]);
+console.log(typeof(favFoods));
+
+// Length property examples
+console.log(favFoods.length);
+console.log(favFoods[3]);
+
+// .push() examples
+favFoods.push("Spaghetti");
+console.log(favFoods);
+
+
+// .pop() examples
+favFoods.pop();
+console.log(favFoods);
+
+//Problem 1
+var daysOfTheWeek =[];
+function problem1(){
+  daysOfTheWeek.push("Monday");
+  daysOfTheWeek.push("Tuesday");
+  daysOfTheWeek.push("Wednesday");
+  daysOfTheWeek.push("Thursday");
+  daysOfTheWeek.push("Friday");
+  daysOfTheWeek.push("Saturday");
+  daysOfTheWeek.push("Sunday");
+  printDays();
+  var weekend = []
+  daysOfTheWeek.pop("Saturday");
+  daysOfTheWeek.pop("Sunday");
+  weekend.push("Saturday");
+  weekend.push("Sunday");
+  printWeekend();
+}
+function printDays(){
+  console.log(daysOfTheWeek[0]);
+  console.log(daysOfTheWeek[1]);
+  console.log(daysOfTheWeek[2]);
+  console.log(daysOfTheWeek[3]);
+  console.log(daysOfTheWeek[4]);
+  console.log(daysOfTheWeek[5]);
+  console.log(daysOfTheWeek[6]);
+}
+printDays();
+
